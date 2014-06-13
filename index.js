fe6df69ec2c0ef206ee16370914c5627a90c5ab2
Kashish Gupta <kashishgupta1990@yahoo.com>
2014-06-13 23:07:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var a = 9;
+var result;
+switch (a) {
+    case 1:
+    {
+        result = 'Number - 1';
+    }
+        break;
+    case 2:
+    {
+        result = 'Number -2';
+    }
+        break;
+    case 9:
+    {
+        result = 'INT - 9';
+    }
+        break;
+    case '9':
+    {
+        result = 'String - 9';
+    }
+        break;
+    default:
+    {
+        result = 'I am default';
+    }
+        break;
+}
+console.log(result);
